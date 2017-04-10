8e80c0d2f897103fdee49ca52905aa23bac7daa3
CanCanZeng <zcc2015@sjtu.edu.cn>
2017-04-10 17:29:38 +0800

    Rearragment of the codes
    I just rearrange the codes and make it more generic to all stereo pictures

--- Dynamic_programming_stereo_matching.m
+++ Dynamic_programming_stereo_matching.m
@@ -7,15 +7,13 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 clc;
 clearvars;
-close all;
+% close all;
 I_1 = imread('frame_left.png'); %Read Left Image
 I_2 = imread('frame_right.png'); %Read Right Image with same size as the Left
-if(size(I_1)==3)
-    I_1 = rgb2gray(imread('frame_left.png'));
-    I_2 = rgb2gray(imread('frame_right.png'));
-else
-    I_1 = imread('mugRectLsample_1_640.pgm');
-    I_2 = imread('mugRectRsample_1_640.pgm');
+[nRow, nCol,nChannel] = size(I_1);
+if(nChannel==3)
+    I_1 = rgb2gray(I_1);
+    I_2 = rgb2gray(I_2);
 end
 I_1 = im2double(I_1);
 I_2 = im2double(I_2);
@@ -23,55 +21,55 @@ I_2 = im2double(I_2);
 figure;imshow(I_1);
 figure;imshow(I_2);
 
-
-C(1,1) = 0;
+C = zeros(nCol, nCol);
+M = ones(size(C));
+displeft = zeros(nRow, nCol);
+dispright = zeros(nRow, nCol);
 occ = 0.0009; %Occlusion cost
-tic;
-for row = 1:480
-    row %To display which row of the image is being processed
-for i=2:640
-    C(i,1) = i*occ;
-end
-for j = 2:640
-   C(1,j) = j*occ; 
-end
-for i = 2:640
-    for j = 2:640
-       temp = (I_1(row,i)-I_2(row,j))^2;
+for row = 1:nRow
+%     row %To display which row of the image is being processed
+    for i=2:nCol
+        C(i,1) = i*occ;
+    end
+    for j = 2:nCol
+       C(1,j) = j*occ; 
+    end
+    for i = 2:nCol
+        for j = 2:nCol
+           temp = (I_1(row,i)-I_2(row,j))^2;
 
-        min1 = C(i-1,j-1)+temp;
-        min2 = C(i-1,j)+occ;
-        min3 = C(i,j-1)+occ;
-        cmin = min([min1,min2,min3]);
-        C(i,j) = cmin; % Cost Matrix
-        if(cmin==min1)
-            M(i,j) = 1; %Path Tracker
-        elseif(cmin==min2)
-            M(i,j) = 2;
-        elseif(cmin==min3)
-            M(i,j) = 3;
+            min1 = C(i-1,j-1)+temp;
+            min2 = C(i-1,j)+occ;
+            min3 = C(i,j-1)+occ;
+            cmin = min([min1,min2,min3]);
+            C(i,j) = cmin; % Cost Matrix
+            if(cmin==min1)
+                M(i,j) = 1; %Path Tracker
+            elseif(cmin==min2)
+                M(i,j) = 2;
+            elseif(cmin==min3)
+                M(i,j) = 3;
+            end
         end
     end
-end
 
-i = 640;
-j = 640;
-while(i~=1 && j~=1)
-    
-   switch M(i,j)
-       case 1
-           displeft(row,i) = abs(i-j); % Disparity Image in Left Image coordinates
-           dispright(row,j) = abs(j-i); % Disparity Image in Right Image coordinates
-           i = i-1;
-           j = j-1;
-       case 2
-           displeft(row,i) = NaN;
-           i = i-1;
-       case 3
-           dispright(row,j) = NaN;
-           j = j-1;
-   end
+    i = nCol;
+    j = nCol;
+    while(i~=1 && j~=1)
+
+       switch M(i,j)
+           case 1
+               displeft(row,i) = abs(i-j); % Disparity Image in Left Image coordinates
+               dispright(row,j) = abs(j-i); % Disparity Image in Right Image coordinates
+               i = i-1;
+               j = j-1;
+           case 2
+               displeft(row,i) = NaN;
+               i = i-1;
+           case 3
+               dispright(row,j) = NaN;
+               j = j-1;
+       end
+    end
+    clear C M
 end
-clear C M
-end
-time_taken = toc;
